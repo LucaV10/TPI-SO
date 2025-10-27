b95c910fd3944bf031eaf81efd00fbea8b5a6490
heberumatta <199393069+heberumatta@users.noreply.github.com>
2025-10-27 10:24:18 -0300

    Remove mostrar_memoria function
    Removed the mostrar_memoria function that displays memory partition details.

--- memoria.py
+++ memoria.py
@@ -18,14 +18,3 @@ def crear_memoria():
     return memoria
 
 
-def mostrar_memoria(memoria):   
-    print("Tabla de Particiones de Memoria:\n")
-    print("Nro | Tamanio | Proceso Asignado | Fragmentacion | Disponible")
-    print("-------------------------------------------------------------")
-  
-    for p in memoria: #El >3 y esas cosas son para darle formato mas lindo
-        estado = "Si" if p['Disponible'] else "No"
-        print(f"{p['NroParticion']:>3} | {p['TamanoParticion']:>6}K | "
-              f"{str(p['ProcesoAsignado']):>15} | "
-              f"{p['FragmentacionInterna']:>13}K | "
-              f"{estado:>11}")
